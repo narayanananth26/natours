efba2942e7d213aca07da4089f96b9988171aac5
Ananth Narayan <narayanananth26@gmail.com>
2023-04-08 00:34:25 +0530

    connect app with database

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 /* eslint-disable no-console */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({
   path: './config.env',
 });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful!');
+});
+
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
