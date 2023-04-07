c143828324df15edb54a6fc890c39a0350811217
Ananth Narayan <narayanananth26@gmail.com>
2023-04-07 11:43:07 +0530

    handle GET one tour request

--- app.js
+++ app.js
@@ -30,6 +30,25 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  id = req.params.id * 1; // Convert string to number
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
