6590c58ec6d483808509f0aa9172aa8af280dbe7
Ananth Narayan <narayanananth26@gmail.com>
2023-04-07 11:34:12 +0530

    handle POST requests

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res.status(404).json({
 //     message: 'Hello from the server side!',
@@ -14,7 +16,9 @@ const app = express();
 //   res.send('You can post to this endpoint!');
 // });
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
@@ -24,6 +28,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      }); // 201 - Created
+    }
+  );
 });
 
 const port = 3000;
