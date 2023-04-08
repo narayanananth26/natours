83b547afc90c43dbc4e7ecb62165d53f9d6ecd6b
Ananth Narayan <narayanananth26@gmail.com>
2023-04-08 13:14:30 +0530

    refactor updateTour handler

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -34,7 +34,6 @@ exports.getTour = async (req, res) => {
       message: err,
     });
   }
-  // const tour = tours.find((el) => el.id === id);
 };
 
 exports.createTour = async (req, res) => {
@@ -55,13 +54,24 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
