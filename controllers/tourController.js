ff1fd22ae293ef803bbe76cd70c7d4ab96b1fa3a
Ananth Narayan <narayanananth26@gmail.com>
2023-04-08 22:12:47 +0530

    add filtering to getAllTours handler

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,19 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr));
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config({
   path: './config.env',
 });
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
