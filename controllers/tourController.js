5bb167b8b4199e2d50f24930056fbf70c0275f24
Ananth Narayan <narayanananth26@gmail.com>
2023-04-13 19:50:54 +0530

    implement /tours/monthly-plan/:year route

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -139,3 +139,80 @@ exports.getTourStats = async (req, res) => {
     });
   }
 };
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: {
+          month: {
+            $let: {
+              vars: {
+                monthsInString: [
+                  ,
+                  'January',
+                  'February',
+                  'March',
+                  'April',
+                  'May',
+                  'June',
+                  'July',
+                  'August',
+                  'September',
+                  'October',
+                  'November',
+                  'December',
+                ],
+              },
+              in: {
+                $arrayElemAt: ['$$monthsInString', '$_id'],
+              },
+            },
+          },
+        },
+      },
+      {
+        $sort: {
+          numTourStarts: -1,
+          _id: 1,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
