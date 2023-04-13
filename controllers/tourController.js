5ec116bd02ff4810258194f8be4eeb20dfbbb5be
Ananth Narayan <narayanananth26@gmail.com>
2023-04-13 15:37:26 +0530

    add sort feature to getAllTours handler

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,12 +5,18 @@ exports.getAllTours = async (req, res) => {
     // BUILD QUERY
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
+    excludedFields.forEach((el) => delete queryObj[el]);
 
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else query = query.sort('-createdAt');
+
     // EXECUTE QUERY
     const tours = await query;
 
