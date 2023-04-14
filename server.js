30a194aa22c06b13a7a2554b04ba436a688f0f34
Ananth Narayan <narayanananth26@gmail.com>
2023-04-14 12:52:25 +0530

    handle Unhandled Rejections

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ mongoose.connect(DB).then(() => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 🧨\nShutting down...');
+  server.close(() => process.exit(1));
 });
