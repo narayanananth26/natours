6454ab195b807e73c24b42bbaecec7bb28fe2012
Ananth Narayan <narayanananth26@gmail.com>
2023-04-14 21:09:39 +0530

    implement /users/login route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,16 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    // 500 - Internal server error
-    status: 'error',
-    message: 'This route is not yet defined!',
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 exports.getUser = (req, res) => {
   res.status(500).json({
     // 500 - Internal server error
--- models/userModel.js
+++ models/userModel.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -33,14 +34,18 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre('save', async function(next){
-  if(!this.isModified('password')) return next();
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
 
   this.password = await bcrypt.hash(this.password, 12);
 
   this.passwordConfirm = undefined;
   next();
-})
+});
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword){
+  return await bcrypt.compare(candidatePassword, userPassword)
+}
 
 const User = mongoose.model('User', userSchema);
 
