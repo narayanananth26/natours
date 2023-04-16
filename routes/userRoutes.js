4482efa3faa02da514cf49ef8fa73ca4b47a89a5
Ananth Narayan <narayanananth26@gmail.com>
2023-04-16 11:22:47 +0530

    add forgot password & reset password feature

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.post('/signup', authController.signup); 
 router.post('/login', authController.login); 
+router.post('/forgot-password', authController.forgotPassword); 
+router.patch('/reset-password/:token', authController.resetPassword); 
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 
