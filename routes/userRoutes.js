49bc6e2746bbb0602a2eb65f6cc6b15633d566bb
Ananth Narayan <narayanananth26@gmail.com>
2023-04-16 12:24:43 +0530

    add update password feature

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,28 @@
 const express = require('express');
-const userController = require('./../controllers/userController')
-const authController = require('./../controllers/authController')
+const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
 
-router.post('/signup', authController.signup); 
-router.post('/login', authController.login); 
-router.post('/forgot-password', authController.forgotPassword); 
-router.patch('/reset-password/:token', authController.resetPassword); 
-
-router.route('/').get(userController.getAllUsers).post(userController.createUser);
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.post('/forgot-password', authController.forgotPassword);
+router.patch('/reset-password/:token', authController.resetPassword);
+router.patch(
+  '/update-my-password',
+  authController.protect,
+  authController.updatePassword
+);
 
 router
-.route('/:id')
-.get(userController.getUser)
-.patch(userController.updateUser)
-.delete(userController.deleteUser);
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
