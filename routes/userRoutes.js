981d20b3be16927e3ac611d5f8d342450c3a2fb0
Ananth Narayan <narayanananth26@gmail.com>
2023-04-18 14:01:16 +0530

    add /user/me endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,10 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+router.get('/me', authController.protect, userController.getMe, userController.getUser);
+router.patch('/update-me', authController.protect, userController.updateMe);
+router.delete('/delete-me', authController.protect, userController.deleteMe);
+
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/forgot-password', authController.forgotPassword);
@@ -13,8 +17,6 @@ router.patch(
   authController.protect,
   authController.updatePassword
 );
-router.patch('/update-me', authController.protect, userController.updateMe);
-router.delete('/delete-me', authController.protect, userController.deleteMe);
 
 router
   .route('/')
