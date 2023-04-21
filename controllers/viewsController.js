0edc0fc6af3145331c4df30e37544eda28fd48b4
Ananth Narayan <narayanananth26@gmail.com>
2023-04-21 12:15:45 +0530

    build tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -10,12 +10,17 @@ exports.getOverview = catchAsync(async (req, res) => {
 
   res.status(200).render('overview', {
     title: 'All Tours',
-    tours
+    tours,
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.tourSlug }).populate({
+    path: 'reviews',
+    fields:'review rating user'
   });
-};
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
